6304e6eedb386ac70b8c50b8811f982a8cf2a857
dhruvi9660 <dh1uvi@gmail.com>
2024-09-27 17:10:18 +0530

    updates

--- app.py
+++ app.py
@@ -45,9 +45,9 @@ def count_check():
     query_to_be_executed = "SELECT * FROM DV_POC_ETL.TEST_RESULT_COUNT;"
     df=run_query(conn, query_to_be_executed)
     print(df.columns.values)
-    col_names = ['SOURCE_TYPE', 'SOURCE_SCHEMA_NAME', 'SOURCE_OBJECT_NAME' ,'TARGET_TYPE',
-    'TARGET_SCHEMA_NAME', 'TARGET_OBJECT_NAME', 
-    'SOURCE_COUNT', 'TARGET_COUNT' ,'SOURCE_SQL', 'TARGET_SQL', 'EXECUTION_DT', 'TEST_RESULT']
+    col_names = ['SOURCE TYPE', 'SOURCE SCHEMA', 'SOURCE OBJECT' ,'TARGET TYPE',
+    'TARGET SCHEMA', 'TARGET OBJECT', 
+    'SOURCE COUNT', 'TARGET COUNT' ,'SOURCE SQL', 'TARGET SQL', 'EXECUTION DT', 'TEST RESULT']
     records = df.to_dict(orient='records')
     return render_template('count_check.html', col_names = col_names, records=records,title='Count Check')
 
@@ -58,8 +58,8 @@ def duplicate_count():
     query_to_be_executed = "SELECT * FROM DV_POC_ETL.TEST_RESULT_DUPLICATE;"
     df =  run_query(conn, query_to_be_executed)
     print(df.columns.values)
-    col_names = ['SOURCE TYPE' ,'SOURCE SCHEMA NAME' ,'SOURCE OBJECT NAME' ,'TARGET TYPE',
- 'TARGET SCHEMA NAME' ,'TARGET OBJECT NAME'  ,'DUPLICATE COUNT',
+    col_names = ['SOURCE TYPE' ,'SOURCE SCHEMA' ,'SOURCE OBJECT' ,'TARGET TYPE',
+ 'TARGET SCHEMA' ,'TARGET OBJECT'  ,'DUPLICATE COUNT',
  'TARGET PRIMARY KEY', 'DUPLICATE SQL' ,'EXECUTION DT','TEST RESULT']
     records = df.to_dict(orient='records')
     return render_template('duplicate_check.html', col_names = col_names, records=records, title='Duplicate Check')
@@ -69,8 +69,8 @@ def duplicate_count():
 def null_percent():
     query_to_be_executed = "SELECT * FROM DV_POC_ETL.TEST_RESULT_NULL_PERCENT;"
     df=run_query(conn, query_to_be_executed)
-    col_names = ['SOURCE TYPE', 'SOURCE SCHEMA NAME', 'SOURCE OBJECT NAME', 'TARGET TYPE',
-    'TARGET SCHEMA NAME' ,'TARGET OBJECT NAME' ,'COLUMN NAME',
+    col_names = ['SOURCE TYPE', 'SOURCE SCHEMA', 'SOURCE OBJECT', 'TARGET TYPE',
+    'TARGET SCHEMA' ,'TARGET OBJECT' ,'COLUMN NAME',
      'NULL COUNT', 'NOT NULL COUNT' ,'TOTAL COUNT' ,'NULL PERCENT', 'EXECUTION DT','TEST RESULT']
     records = df.to_dict(orient='records')
     return render_template('null_percent.html', col_names = col_names, records=records,title='Null Percent Check')
