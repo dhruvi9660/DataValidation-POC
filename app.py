09972db421c174c620de8228f909461294285eeb
dhruvi9660 <dh1uvi@gmail.com>
2024-09-27 18:58:48 +0530

    updates

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import snowflake.connector as sf
 from flask import Flask, request
 from flask import *
 from flask import *
+from datetime import datetime
 
 app=Flask(__name__)
 user="pocValidation"
@@ -44,10 +45,12 @@ def hello_world():
 def count_check():
     query_to_be_executed = "SELECT * FROM DV_POC_ETL.TEST_RESULT_COUNT;"
     df=run_query(conn, query_to_be_executed)
-    print(df.columns.values)
+    # print(df.columns.values)
+    for i in range(len(df)):
+        df['EXECUTION_DT'][i] = df['EXECUTION_DT'][i].strftime("%Y-%m-%d %H:%M:%S")
     col_names = ['SOURCE TYPE', 'SOURCE SCHEMA', 'SOURCE OBJECT' ,'TARGET TYPE',
     'TARGET SCHEMA', 'TARGET OBJECT', 
-    'SOURCE COUNT', 'TARGET COUNT' ,'SOURCE SQL', 'TARGET SQL', 'EXECUTION DT', 'TEST RESULT']
+    'SOURCE COUNT', 'TARGET COUNT' ,'SOURCE SQL', 'TARGET SQL', 'EXECUTION DATE', 'TEST RESULT']
     records = df.to_dict(orient='records')
     return render_template('count_check.html', col_names = col_names, records=records,title='Count Check')
 
@@ -57,10 +60,12 @@ def count_check():
 def duplicate_count():
     query_to_be_executed = "SELECT * FROM DV_POC_ETL.TEST_RESULT_DUPLICATE;"
     df =  run_query(conn, query_to_be_executed)
-    print(df.columns.values)
+    # print(df.columns.values)
+    for i in range(len(df)):
+        df['EXECUTION_DT'][i] = df['EXECUTION_DT'][i].strftime("%Y-%m-%d %H:%M:%S")
     col_names = ['SOURCE TYPE' ,'SOURCE SCHEMA' ,'SOURCE OBJECT' ,'TARGET TYPE',
  'TARGET SCHEMA' ,'TARGET OBJECT'  ,'DUPLICATE COUNT',
- 'TARGET PRIMARY KEY', 'DUPLICATE SQL' ,'EXECUTION DT','TEST RESULT']
+ 'TARGET PRIMARY KEY', 'DUPLICATE SQL' ,'EXECUTION DATE','TEST RESULT']
     records = df.to_dict(orient='records')
     return render_template('duplicate_check.html', col_names = col_names, records=records, title='Duplicate Check')
 
@@ -69,9 +74,11 @@ def duplicate_count():
 def null_percent():
     query_to_be_executed = "SELECT * FROM DV_POC_ETL.TEST_RESULT_NULL_PERCENT;"
     df=run_query(conn, query_to_be_executed)
+    for i in range(len(df)):
+        df['EXECUTION_DT'][i] = df['EXECUTION_DT'][i].strftime("%Y-%m-%d %H:%M:%S")
     col_names = ['SOURCE TYPE', 'SOURCE SCHEMA', 'SOURCE OBJECT', 'TARGET TYPE',
     'TARGET SCHEMA' ,'TARGET OBJECT' ,'COLUMN NAME',
-     'NULL COUNT', 'NOT NULL COUNT' ,'TOTAL COUNT' ,'NULL PERCENT', 'EXECUTION DT','TEST RESULT']
+     'NULL COUNT', 'NOT NULL COUNT' ,'TOTAL COUNT' ,'NULL PERCENT', 'EXECUTION DATE','TEST RESULT']
     records = df.to_dict(orient='records')
     return render_template('null_percent.html', col_names = col_names, records=records,title='Null Percent Check')
 
