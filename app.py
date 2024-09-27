0313ee52e521569db81e88489f23d2c6fb8b1eb0
dhruvi9660 <dh1uvi@gmail.com>
2024-09-27 16:04:45 +0530

    all three pages added

--- app.py
+++ app.py
@@ -40,10 +40,16 @@ def hello_world():
     return render_template('index.html')
 
 
-app.route('/count', methods=['GET','POST'])
-def count():
+@app.route('/count_check', methods=['GET','POST'])
+def count_check():
     query_to_be_executed = "SELECT * FROM DV_POC_ETL.TEST_RESULT_COUNT;"
-    return run_query(conn, query_to_be_executed)
+    df=run_query(conn, query_to_be_executed)
+    print(df.columns.values)
+    col_names = ['SOURCE_TYPE', 'SOURCE_SCHEMA_NAME', 'SOURCE_OBJECT_NAME' ,'TARGET_TYPE',
+    'TARGET_SCHEMA_NAME', 'TARGET_OBJECT_NAME', 
+    'SOURCE_COUNT', 'TARGET_COUNT' ,'SOURCE_SQL', 'TARGET_SQL', 'EXECUTION_DT', 'TEST_RESULT']
+    records = df.to_dict(orient='records')
+    return render_template('count_check.html', col_names = col_names, records=records,title='Count Check')
 
 
 
@@ -51,15 +57,23 @@ def count():
 def duplicate_count():
     query_to_be_executed = "SELECT * FROM DV_POC_ETL.TEST_RESULT_DUPLICATE;"
     df =  run_query(conn, query_to_be_executed)
-    col_names = [ 'SCHEMA_NAME', 'TABLE_NAME', 'TEST_RESULT', 'DUPLICATE_COUNT', 'PRIMARY_KEY', 'DUPLICATE_SQL', 'INSERT_DT']
+    print(df.columns.values)
+    col_names = ['SOURCE TYPE' ,'SOURCE SCHEMA NAME' ,'SOURCE OBJECT NAME' ,'TARGET TYPE',
+ 'TARGET SCHEMA NAME' ,'TARGET OBJECT NAME'  ,'DUPLICATE COUNT',
+ 'TARGET PRIMARY KEY', 'DUPLICATE SQL' ,'EXECUTION DT','TEST RESULT']
     records = df.to_dict(orient='records')
-    return render_template('index.html', col_names = col_names, records=records)
+    return render_template('duplicate_check.html', col_names = col_names, records=records, title='Duplicate Check')
 
 
 @app.route('/null_percent', methods=['GET','POST'])
 def null_percent():
     query_to_be_executed = "SELECT * FROM DV_POC_ETL.TEST_RESULT_NULL_PERCENT;"
-    return run_query(conn, query_to_be_executed)
+    df=run_query(conn, query_to_be_executed)
+    col_names = ['SOURCE TYPE', 'SOURCE SCHEMA NAME', 'SOURCE OBJECT NAME', 'TARGET TYPE',
+    'TARGET SCHEMA NAME' ,'TARGET OBJECT NAME' ,'COLUMN NAME',
+     'NULL COUNT', 'NOT NULL COUNT' ,'TOTAL COUNT' ,'NULL PERCENT', 'EXECUTION DT','TEST RESULT']
+    records = df.to_dict(orient='records')
+    return render_template('null_percent.html', col_names = col_names, records=records,title='Null Percent Check')
 
 if __name__=='__main__':
     app.run(debug=True)
